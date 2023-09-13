0b76d2e10842744e99ad8ef1706155d7a213d612
bryan <bryan@mightybeargames.com>
2023-09-13 10:23:36 +0800

    Cherrypick from main branch

--- dist/default-build-script/Assets/Editor/UnityBuilderAction/Input/AndroidSettings.cs
+++ dist/default-build-script/Assets/Editor/UnityBuilderAction/Input/AndroidSettings.cs
@@ -9,11 +9,13 @@ namespace UnityBuilderAction.Input
     public static void Apply(Dictionary<string, string> options)
     {
       EditorUserBuildSettings.buildAppBundle = options["customBuildPath"].EndsWith(".aab");
+#if UNITY_2019_1_OR_NEWER
       if (options.TryGetValue("androidKeystoreName", out string keystoreName) && !string.IsNullOrEmpty(keystoreName))
       {
         PlayerSettings.Android.useCustomKeystore = true;
         PlayerSettings.Android.keystoreName = keystoreName;
       }
+#endif
       if (options.TryGetValue("androidKeystorePass", out string keystorePass) && !string.IsNullOrEmpty(keystorePass))
         PlayerSettings.Android.keystorePass = keystorePass;
       if (options.TryGetValue("androidKeyaliasName", out string keyaliasName) && !string.IsNullOrEmpty(keyaliasName))
@@ -21,7 +23,19 @@ namespace UnityBuilderAction.Input
       if (options.TryGetValue("androidKeyaliasPass", out string keyaliasPass) && !string.IsNullOrEmpty(keyaliasPass))
         PlayerSettings.Android.keyaliasPass = keyaliasPass;
       if (options.TryGetValue("androidTargetSdkVersion", out string androidTargetSdkVersion) && !string.IsNullOrEmpty(androidTargetSdkVersion))
-        PlayerSettings.Android.targetSdkVersion = (AndroidSdkVersions) Enum.Parse(typeof(AndroidSdkVersions), androidTargetSdkVersion);
+      {
+          var targetSdkVersion = AndroidSdkVersions.AndroidApiLevelAuto;
+          try
+          {
+              targetSdkVersion =
+                  (AndroidSdkVersions) Enum.Parse(typeof(AndroidSdkVersions), androidTargetSdkVersion);
+          }
+          catch
+          {
+              UnityEngine.Debug.Log("Failed to parse androidTargetSdkVersion! Fallback to AndroidApiLevelAuto");
+          }
+          PlayerSettings.Android.targetSdkVersion = targetSdkVersion;
+      }
     }
   }
 }
